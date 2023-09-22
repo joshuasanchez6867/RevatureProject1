437d05a62ae97f503a74586957b31d91af73df80
joshuasanchez6867 <jossanchez@ucdavis.edu>
2023-09-22 14:07:17 -0700

    fixed ticekt not existing error

--- src/js/Service/ticketService.js
+++ src/js/Service/ticketService.js
@@ -46,44 +46,49 @@ const arbitrate = (req, res) => {
     if(req.body.ticket_id == null || typeof req.body.ticket_id  !== typeof 'string'|| req.body.ticket_id == undefined){//add for other memebrs too
         res.status(400).send("Ticket ID Invalid");
     }
-    if( req.body.decision !== "APPROVED" &&  req.body.decision !== "DENIED"){//add for other memebrs too
+    else if( req.body.decision !== "APPROVED" &&  req.body.decision !== "DENIED"){//add for other memebrs too
         res.status(400).send("Decision Invalid: must be APPROVED or DENIED");
     }
-    const token = req.headers.authorization.split(' ')[1]; // ['Bearer', '<token>'];
+    else{
+        const token = req.headers.authorization.split(' ')[1]; // ['Bearer', '<token>'];
 
-    jwtUtil.verifyTokenAndReturnPayload(token)
-    .then((payload) => {
-        if(payload.admin === 'admin') {
-            TicketDAO.getTicketByIDDAO(req.body.ticket_id)
-            .then((data) => {
-                if(data.Item.status !== 'PENDING'){
-                    res.status(403).send('Cannot change tickets that have already been decided on');
-                }
-                else{
-                    TicketDAO.arbitrateTicketDAO(req.body.ticket_id, req.body.decision, payload.username)
-                    .then(() => {
-                        res.status(200).send('Succesfully Updated');
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        res.status(400).send('Bad Request');
-                    })
-                }
+        jwtUtil.verifyTokenAndReturnPayload(token)
+        .then((payload) => {
+            if(payload.admin === 'admin') {
+                TicketDAO.getTicketByIDDAO(req.body.ticket_id)
+                .then( (data) => {
+                    if(data.Item === undefined){
+                        res.status(400).send('Ticket Does NOT Exist');
+                    }
+                    else if(data.Item.status !== 'PENDING'){
+                        res.status(403).send('Cannot change tickets that have already been decided on');
+                    }
+                    else{
+                        TicketDAO.arbitrateTicketDAO(req.body.ticket_id, req.body.decision, payload.username)
+                        .then(() => {
+                            res.status(200).send('Succesfully Updated');
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            res.status(400).send('Bad Request');
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(400).send('Ticket ID causes error')
+                })
+            }
+            else {
+                res.status(403).send({message: `You are not an Administrator, you are an Employee`})
+            }
+        }).catch((err) => {
+            console.error(err);
+            res.statusCode(401).send({
+                message: "Failed to Authenticate Token"
             })
-            .catch((err) => {
-                console.log(err);
-                res.status(400).send('Ticket Does NOT Exist')
-            })
+        }) 
         }
-        else {
-            res.status(403).send({message: `You are not an Administrator, you are an Employee`})
-        }
-    }).catch((err) => {
-        console.error(err);
-        res.statusCode(401).send({
-            message: "Failed to Authenticate Token"
-        })
-    }) 
 }
 const viewTickets = (req, res) => {
 
